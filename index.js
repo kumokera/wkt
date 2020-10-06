9301090ff69a65edf9e070438433d345b3e73ff3
kumokera <kumo.kera@gmail.com>
2020-10-06 14:05:40 +0900

    js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.write('<h1>Hello World</h1>');
-    res.end();
-});
-const port = 8080;
-server.listen(port);
-console.log('Server listen on port ' + port);
+    res.end('Hello World\n');
+}).listen(3000);
+//const port = 8080;
+//server.listen(port);
+console.log('Server listen on port ' + 3000);
 
